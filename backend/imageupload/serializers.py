33a7a5aed406496fce4d0afdec1783579851c1ca
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2023-01-29 15:07:53 -0500

    finish backend api

---
+++ backend/imageupload/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import ImageUpload
+
+
+class ImageUploadSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    image = serializers.ImageField()
+
+    def create(self, validated_data):
+        return ImageUpload.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+        return instance
+
+    # class Meta:
+    #     model = ImageUpload
+    #     fields = ['image']  
+
+# {"image":"image field"}
--- backend/imageupload/views.py
+++ backend/imageupload/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import ImageUpload
+from .serializers import ImageUploadSerializer
 
 # Create your views here.
+
+class ImageUploadViewSet(viewsets.ModelViewSet):
+  queryset = ImageUpload.objects.all()
+  serializer_class = ImageUploadSerializer
